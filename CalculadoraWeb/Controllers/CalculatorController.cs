0f1c0b4e75eb68351f15b5d2c7c08c46dd040575
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-31 18:04:42 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraWeb/Controllers/CalculatorController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+
+        //outras operações (get)
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string number)
+        {
+            double n;
+            bool isNumber = double.TryParse(number, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out n);
+
+            return isNumber;
+            
+        }
+    }
+}
